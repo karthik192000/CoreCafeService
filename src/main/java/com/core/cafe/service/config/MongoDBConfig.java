a701507317c0ea69e367490e8e9a8078c195b964
Karthik Bankapur <karthik.bankapur@bajajfinserv.in>
2024-03-29 21:55:40 +0530

    enabled mongo

--- src/main/java/com/core/cafe/service/config/MongoDBConfig.java
+++ src/main/java/com/core/cafe/service/config/MongoDBConfig.java
@@ -1,48 +1,48 @@
-//package com.core.cafe.service.config;
-//
-//
-//import com.mongodb.ConnectionString;
-//import com.mongodb.MongoClientSettings;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//
-////@Configuration
-//public class MongoDBConfig {
-//
-//
-//
-//
-//    @Bean
-//    public MongoClient mongoClient(){
-//        MongoClient mongoClient = null;
-//        try {
-//            ConnectionString connectionString = new ConnectionString("mongo://localhost:27017/test");
-//            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-//                    .applyConnectionString(connectionString)
-//                    .build();
-//            mongoClient = MongoClients.create(mongoClientSettings);
-//        }
-//        catch (Exception ex){
-//            ex.printStackTrace();
-//        }
-//        return mongoClient;
-//    }
-//
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate(){
-//        MongoTemplate mongoTemplate = null;
-//        try {
-//            mongoTemplate = new MongoTemplate(mongoClient(), "test");
-//        }
-//        catch (Exception ex){
-//            ex.printStackTrace();
-//        }
-//
-//        return mongoTemplate;
-//
-//    }
-//}
+package com.core.cafe.service.config;
+
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoDBConfig {
+
+
+
+
+    @Bean
+    public MongoClient mongoClient(){
+        MongoClient mongoClient = null;
+        try {
+            ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/cafe");
+            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            mongoClient = MongoClients.create(mongoClientSettings);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return mongoClient;
+    }
+
+
+    @Bean
+    public MongoTemplate mongoTemplate(){
+        MongoTemplate mongoTemplate = null;
+        try {
+            mongoTemplate = new MongoTemplate(mongoClient(), "cafe");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return mongoTemplate;
+
+    }
+}
